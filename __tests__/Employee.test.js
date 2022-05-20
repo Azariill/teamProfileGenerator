c68ec40991504a7456b8814c5de5e52627e8a46a
Azarill <heierms@gmail.com>
2022-05-20 00:23:16 -0500

    added starter code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,41 @@
+
+const Employee = require('../lib/Employee');
+
+
+
+// run a test to verify that ea class is creating and applying properties correctly
+test('create a new employee with expected values', ()=>{
+    const employee = new Employee('Scott', 1051615, 'heierms@gmail.com');
+   
+    // Test employee creation 
+    expect(employee.name).toBe('Scott');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('heierms@gmail.com');
+});
+
+test('test get name method', () => {
+    
+    const employee = new Employee('Scott','1051615', 'heierms@gmail.com');
+      
+    expect(employee.getName()).toBe('Scott');
+
+});
+
+test('test getId method return number', ()=>{
+    const employee = new Employee('Scott', 1151, 'heierms@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+
+})
+
+test('test getEmail() method to return email', () =>{
+    const employee = new Employee('Scott', 1151, 'heierms@gmail.com');
+
+    expect(employee.getEmail()).toEqual(employee.email);
+})
+
+test('make sure that getRole() returns employee', () =>{
+    const employee = new Employee('Scott', 11115, 'heierms@gmail.com');
+
+    expect(employee.getRole()).toBe('Employee');
+})
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require('../lib/Inter.js');
+
+
+test('create a new employee with expected values', ()=>{
+    const employee = new Employee('Scott', 1051615, 'heierms@gmail.com');
+    const manager = new Manager('George',01, 'gmanager@gmail.com');
+    const engineer = new Engineer('James', 02, 'eng@gmail.com');
+    const intern = new Intern('Sevro',05,'sevro@gmail.com');
+
+    // Test employee creation 
+    expect(employee.name).toBe('Scott');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('heierms@gmail.com');
+    //test manager creation 
+    expect(manager.name).toBe('George');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('gmanager@gmail.com');
+    //test engineer creation
+    expect(engineer.name).toBe('James');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('eng@gmail.com');
+    //test intern creation
+    expect(engineer.name).toBe('James');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('eng@gmail.com');
+
+});
